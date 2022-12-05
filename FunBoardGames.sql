d2e5516e2fc39f2ed0a707d0c14249cbf53ed06e
ThessaEric <104193051+ThessaEric@users.noreply.github.com>
2022-12-05 18:04:31 +0100

    Update FunBoardGames.sql

--- FunBoardGames.sql
+++ FunBoardGames.sql
@@ -46,23 +46,25 @@ name_contact VARCHAR(40),
 mail_supplier varchar(50)
 );
 
-INSERT INTO Game VALUES(01,100,50,'?',5);
-INSERT INTO Game VALUES(02,100,50,'?',5);
-INSERT INTO Game VALUES(03,100,50,'?',5);
-INSERT INTO Game VALUES(04,100,50,'?',5);
-INSERT INTO Game VALUES(05,100,50,'?',5);
-INSERT INTO Game VALUES(06,100,50,'?',5);
-INSERT INTO Game VALUES(07,100,50,'?',5);
-INSERT INTO Game VALUES(08,100,50,'?',5);
-INSERT INTO Game VALUES(09,100,50,'?',5);
-INSERT INTO Game VALUES(10,100,50,'?',5);
-INSERT INTO Game VALUES(11,100,50,'?',5);
-INSERT INTO Game VALUES(12,100,50,'?',5);
-INSERT INTO Game VALUES(13,100,50,'?',5);
-INSERT INTO Game VALUES(14,100,50,'?',5);
-INSERT INTO Game VALUES(15,100,50,'?',5);
-INSERT INTO Game VALUES(16,100,50,'?',5);
-INSERT INTO Game VALUES(17,100,50,'?',5);
-INSERT INTO Game VALUES(18,100,50,'?',5);
-INSERT INTO Game VALUES(19,100,50,'?',5);
-INSERT INTO Game VALUES(20,100,50,'?',5);
+INSERT INTO Game VALUES('Monopoly',01,100,50,'?',5,"https://images.pexels.com/photos/4004420/pexels-photo-4004420.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2");
+INSERT INTO Game VALUES('Uno',02,100,50,'?',5,"https://images.pexels.com/photos/2689343/pexels-photo-2689343.jpeg?auto=compress&cs=tinysrgb&w=600");
+INSERT INTO Game VALUES('Loup garou',03,100,50,'?',5,"https://images.king-jouet.com/6/gu238003_6.jpg");
+INSERT INTO Game VALUES('Bonne paye',04,100,50,'?',5,"https://www.regles-de-jeux.com/wp-content/uploads/2012/11/bonne-paie1.jpg");
+INSERT INTO Game VALUES('Catan',05,100,50,'?',5,"https://c8.alamy.com/compfr/rap20n/jeu-de-fete-avec-mes-amis-les-colons-de-catane-jeu-de-conseil-populaire-les-joueurs-se-bousculent-la-region-pour-obtenir-plus-de-ressources-et-de-points-de-victoire-rap20n.jpg");
+INSERT INTO Game VALUES('Cluedo',06,100,50,'?',5,"http://gusandco.net/wp-content/uploads/2011/04/241_1.jpg");
+INSERT INTO Game VALUES('Echecs',07,100,50,'?',5,"https://images.pexels.com/photos/6688954/pexels-photo-6688954.jpeg?auto=compress&cs=tinysrgb&w=600");
+INSERT INTO Game VALUES('Scrabble',08,100,50,'?',5,"https://images.pexels.com/photos/256417/pexels-photo-256417.jpeg?auto=compress&cs=tinysrgb&w=600");
+INSERT INTO Game VALUES('Trivial Poursuit',09,100,50,'?',5,"https://st3.depositphotos.com/1263499/33737/i/600/depositphotos_337373828-stock-photo-trivial-pursuit-board-game-80s.jpg");
+INSERT INTO Game VALUES('Puissance 4',10,100,50,'?',5);
+INSERT INTO Game VALUES('Dominos',11,100,50,'?',5);
+INSERT INTO Game VALUES('Triomino',12,100,50,'?',5);
+INSERT INTO Game VALUES('Risk',13,100,50,'?',5);
+INSERT INTO Game VALUES('Docteur Maboule',14,100,50,'?',5);
+INSERT INTO Game VALUES('Dames',15,100,50,'?',5);
+INSERT INTO Game VALUES('Milles Bornes',16,100,50,'?',5);
+INSERT INTO Game VALUES("Jeu de l'Oie",17,100,50,'?',5);
+INSERT INTO Game VALUES('Mastermind',18,100,50,'?',5);
+INSERT INTO Game VALUES('Les petits chevaux',19,100,50,'?',5);
+INSERT INTO Game VALUES("Qui est ce",20,100,50,'?',5);
+
+
